c247bf8a1064aa9c108a5d13a99446bba67c72d6
Ravi <rranjankumarprajapati@gmail.com>
2025-05-15 21:13:27 +0530

    feat: added signin cards

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,9 +1,9 @@
+"use client";
+
+import { SignUpCard } from "@/features/auth/components/sign-up-card";
+
 const SignUpPage = () => {
-    return (
-        <div>
-            SignUp Page
-        </div>
-    );
+    return <SignUpCard />
 }
 
 export default SignUpPage;
